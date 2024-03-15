1c8fead6054e0c1781f784e67bc9bd5b9e90ae49
preethi415 <preethib2002@gmail.com>
2024-03-15 13:46:41 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def revi(request):
+    return render(request,'revi.html')
+
+
+
+def srujana(request):
+    return render(request,'srujana.html')
